1a9187da82910067d74ca7d95805ab25a5523d24
coleb <cole.ballard97@gmail.com>
2021-01-07 14:16:38 -0600

    added score display and initial input with localStorage keeping

--- script.js
+++ script.js
@@ -1,6 +1,89 @@
 const TOTAL_SECONDS = 180, TIME_PENALTY = 15;
 var secondsElapsed, interval, countdown, countdownInterval;
 
+const QUESTIONS_COPY = [
+  {
+    question: "What attribute is used in the '<script>' tag to link to a Javascript file?",
+    answers: ["src", "href", "a", "aria"]
+  },
+  {
+    question: "Who created Javascript while working for Netscape in 1995?",
+    answers: ["Brandon Eich", "Bjarne Stroustrup", "James Gosling", "Tim Berners-Lee"]
+  },
+  {
+    question: "What pair of symbols follows the end of a function?",
+    answers: ["()", "[]", "{}", "<>"]
+  },
+  {
+    question: "What's the main difference in how '===' and '==' compare values?",
+    answers: ["type", "int", "assignment", "iteration"]
+  },
+  {
+    question: "What data type is to be expected in the parenthesis after a 'while' loop?",
+    answers: ["boolean", "integer", "string", "object"]
+  },
+  {
+    question: "Which of these is a correct implementation of a 'for' loop?",
+    answers: ["for (var i = 0; i <= 10; i++) { ... }", "for (i > 10) { ... }", "for (var i = 0; i == true ; i++) { ... }", "for (i + i * 10) { ... }"]
+  },
+  {
+    question: "Which of these is a correct implementation of a single-line comment in Javascript?",
+    answers: ["// comment", "/ comment /", "/- comment -/", "/-/ comment /-/"]
+  },
+  {
+    question: "What pair of symbols is used to denote an array?",
+    answers: ["[]", "()", "{}", "<>"]
+  },
+  {
+    question: "What will 'Math.random()' return?",
+    answers: ["a floating value between 0 and 1", "undefined", "an integer between 1 and 10", "an integer between 0 and 9"]
+  },
+  {
+    question: "What company currently owns the rights to Javascript?",
+    answers: ["Oracle", "Google", "International Organization for Standardization", "Microsoft"]
+  },
+  {
+    question: "Which of these is not a variable keyword in Javascript?",
+    answers: ["int", "var", "let", "const"]
+  },
+  {
+    question: "What operator is used to perform multiplication?",
+    answers: ["*", "x", "#", "multiply()"]
+  },
+  {
+    question: "Which one of these statements is correct?",
+    answers: ["Javascript is case-sensitive and ignores whitespace", "Javascript is not case-sensitive and ignores whitespace", "Javascript is case-sensitive and doesn't ignore whitespace", "Javascript is not case-sensitive and doesn't ignore whitespace"]
+  },
+  {
+    question: "Which of these statements is correct?",
+    answers: ["Javascript is a weakly and dynamically typed language", "Javascript is a strongly and dynamically typed language", "Javascript is a weakly and non-dynamically typed language", "Javascript is a strongly and non-dynamically typed language"]
+  },
+  {
+    question: "Which function returns the character at a specified index?",
+    answers: ["charAt()", "indexOf()", "getChar()", "getIndex()"]
+  },
+  {
+    question: "Is Javascript multi-threaded?",
+    answers: ["no", "yes", "maybe", "so"]
+  },
+  {
+    question: "Is 'null' an object?",
+    answers: ["yes", "no", "maybe", "so"]
+  },
+  {
+    question: "Which of these is a Javascript file extension?",
+    answers: [".js", ".jvs", ".javascript", ".ajt"]
+  },
+  {
+    question: "What Javascript function is used to get an HTML DOM element with a specified ID?",
+    answers: ["getElementByID()", "getElement()", "id()", "retrieveElement()"]
+  },
+  {
+    question: "if/else and switch/case are both conditional statements. What is the if/else equivalent of 'default:'?",
+    answers: ["else", "break", "continue", "boolean"]
+  }
+];
+
 var questions = [
   {
     question: "What attribute is used in the '<script>' tag to link to a Javascript file?",
@@ -112,7 +195,7 @@ function startCountdown() {
 
 function stopTimer() {
   clearInterval(interval);
-  // call score function
+  displayScore();
 }
 
 
@@ -182,10 +265,38 @@ function displayAnswerFeedback(correct) {
       if ($('#quiz-question').text().localeCompare(questions[x].question) == 0) 
         deleteIndexFromQuestions(x);
     }
+    
     if (questions.length != 0)
       nextQuestion();
-    
+    else stopTimer();
   }, 1000);
+}
+
+function displayScore() {
+  var score = TOTAL_SECONDS - secondsElapsed;
+
+  $('#quiz-page-body-quiz').css('display', 'none');
+  $('#score-enter-initials').css('display', 'block');
+
+  $('#score-text').text(score);
+}
+
+function addScore() {
+  var userScore = TOTAL_SECONDS - secondsElapsed;
+  var initials = $('#initials-input').val();
+  var scores;
+
+  if (!localStorage.getItem('scores')) {
+    scores = {[initials] : userScore};
+    localStorage.setItem('scores', JSON.stringify(scores));
+  }
+  else {
+    scores = JSON.parse(localStorage.getItem('scores'));
+    scores[initials] = userScore;
+    localStorage.setItem('scores', JSON.stringify(scores));
+  }
+  
+  makeQuestionsDefault();
 }
 
 function deleteIndexFromQuestions(index) {
@@ -201,11 +312,20 @@ function deleteIndexFromQuestions(index) {
   for (var y = 0; y <= (length - 1); y++) {
     questions.pop();
   }
-  console.log(newArray);
+  
   for (var z = 0; z <= (length - 2); z++) {
     questions.push(newArray[z]);
   }
-  console.log(questions);
+}
+
+function makeQuestionsDefault() {
+  var length = questions.length;
+
+  for (var i = 0; i <= (length - 1); i++)
+    questions.pop();
+
+  for (var j = 0; j <= (QUESTIONS_COPY.length - 1); j++) 
+    questions.push(QUESTIONS_COPY[j]);
 }
 
 function stopCountdown() {
@@ -249,7 +369,10 @@ $('#quiz-page-body-start .btn').click(function() {
   startCountdown();
 });
 
-
 $('#quiz-page-body-quiz .btn').click(function() {
   checkAnswer(this);
-}) ;
+});
+
+$('#score-enter-initials .btn').click(function() {
+  addScore();
+});

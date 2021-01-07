adea67f415f22a9a8ae63badec4f724a653d910b
coleb <cole.ballard97@gmail.com>
2021-01-07 04:12:52 -0600

    added quiz start page, countdown, and template for questions

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $('#quiz-page header .btn').click(function() {
-  console.log('test');
   $('#quiz-page').css('display', 'none');
   $('#highscores-page').css('display', 'block');
 });
@@ -8,3 +7,23 @@ $('#highscores-page header .btn').click(function() {
   $('#highscores-page').css('display', 'none');
   $('#quiz-page').css('display', 'block');
 });
+
+$('#quiz-page-body-start .btn').click(function() {
+  $('#quiz-page-body-start').css('display', 'none');
+  $('#quiz-page-body-countdown').css('display', 'block');
+
+  var count = 3;
+  $('#quiz-countdown').text(count--);
+
+  setInterval(function() {
+    $('#quiz-countdown').text(count--);
+    if (count == 0) 
+      startQuiz();
+  }, 1000);
+
+});
+
+function startQuiz() {
+  $('#quiz-page-body-countdown').css('display', 'none');
+  $('#quiz-page-body-quiz').css('display', 'block');
+}

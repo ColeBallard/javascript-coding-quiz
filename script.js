e2718229529a04cdcc8c80b956a58d4aa38047eb
coleb <cole.ballard97@gmail.com>
2021-01-07 05:16:31 -0600

    added timer

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
+var secondsElapsed, interval;
+const TOTAL_SECONDS = 180, TIME_PENALTY = 15;
+
+function startQuiz() {
+  $('#quiz-page-body-countdown').css('display', 'none');
+  $('#quiz-page-body-quiz').css('display', 'block');
+
+  startTimer();
+}
+
+function startCountdown() {
+  var timer = 3;
+  $('#quiz-countdown').text(timer--);
+
+  var countdown = setInterval(function() {
+    $('#quiz-countdown').text(timer--);
+    if (timer == -2) {
+      clearInterval(countdown); 
+      startQuiz();
+  }
+  }, 1000);
+}
+
+function startTimer() {
+  secondsElapsed = 0;
+
+  interval = setInterval(function() {
+    secondsElapsed++;
+    renderTimer();
+  }, 1000);
+}
+
+function stopTimer() {
+  clearInterval(interval);
+}
+
+
+function renderTimer() {
+  var seconds = TOTAL_SECONDS - secondsElapsed;
+  var minutes = Math.floor(seconds / 60);
+
+  seconds %= 60;
+  if (seconds <= 9) seconds = "0" + seconds;
+
+  $('#time-left-text').text(minutes + ":" + seconds);
+
+  if (secondsElapsed >= TOTAL_SECONDS) stopTimer();
+}
+
+function decreaseTimer() {
+  secondsElapsed -= TIME_PENALTY;
+}
+
+
 $('#quiz-page header .btn').click(function() {
   $('#quiz-page').css('display', 'none');
   $('#highscores-page').css('display', 'block');
@@ -11,19 +65,5 @@ $('#highscores-page header .btn').click(function() {
 $('#quiz-page-body-start .btn').click(function() {
   $('#quiz-page-body-start').css('display', 'none');
   $('#quiz-page-body-countdown').css('display', 'block');
-
-  var count = 3;
-  $('#quiz-countdown').text(count--);
-
-  setInterval(function() {
-    $('#quiz-countdown').text(count--);
-    if (count == 0) 
-      startQuiz();
-  }, 1000);
-
+  startCountdown();
 });
-
-function startQuiz() {
-  $('#quiz-page-body-countdown').css('display', 'none');
-  $('#quiz-page-body-quiz').css('display', 'block');
-}
